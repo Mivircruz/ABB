c2f343316f83dc300de65888c8810de7b6d6f10d
Milagros Cruz <mivircruz@gmail.com>
2018-11-08 01:34:07 -0300

    pruebas que pasa hasta ahora

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -21,6 +21,7 @@ void pruebas_abb_vacio(void){
 	print_test("Borrar elemento A en abb vacío es NULL\n", !abb_borrar(abb, "A"));
 	print_test("Obtener elemento A en abb vacío es NULL", !abb_obtener(abb, "A"));
 	print_test("Elemento A pertenece a abb es falso", !abb_pertenece(abb, "A"));
+	print_test("Borrar elemento A es NULL", !abb_borrar(abb, "A"));
 
 	abb_destruir(abb);
 }
@@ -42,19 +43,17 @@ static void pruebas_abb_insertar(){
     print_test("Prueba abb la cantidad de elementos es 1", abb_cantidad(abb) == 1);
     print_test("Prueba abb obtener clave1 es valor1", abb_obtener(abb, clave1) == valor1);
     print_test("Prueba abb pertenece clave1, es true", abb_pertenece(abb, clave1));
-   // print_test("Prueba abb borrar clave1, es valor1", abb_borrar(abb, clave1) == valor1);
-   // print_test("Prueba abb la cantidad de elementos es 0", abb_cantidad(abb) == 0);
+   	print_test("Prueba abb borrar clave1, es valor1", abb_borrar(abb, clave1) == valor1);
+   	print_test("Prueba abb la cantidad de elementos es 0", abb_cantidad(abb) == 0);
 
     /* Inserta otros 2 valores y no los borra (se destruyen con el abb) */
     print_test("Prueba abb insertar clave2", abb_guardar(abb, clave2, valor2));
-  	print_test("Prueba abb la cantidad de elementos es 2", abb_cantidad(abb) == 2);
-    print_test("Prueba abb obtener clave2 es valor2", abb_obtener(abb, clave2) == valor2);
+  	print_test("Prueba abb la cantidad de elementos es 1", abb_cantidad(abb) == 1);
     print_test("Prueba abb obtener clave2 es valor2", abb_obtener(abb, clave2) == valor2);
     print_test("Prueba abb pertenece clave2, es true", abb_pertenece(abb, clave2));
 
     print_test("Prueba abb insertar clave3", abb_guardar(abb, clave3, valor3));
-  	print_test("Prueba abb la cantidad de elementos es 3", abb_cantidad(abb) == 3);
-    print_test("Prueba abb obtener clave3 es valor3", abb_obtener(abb, clave3) == valor3);
+  	print_test("Prueba abb la cantidad de elementos es 2", abb_cantidad(abb) == 2);
     print_test("Prueba abb obtener clave3 es valor3", abb_obtener(abb, clave3) == valor3);
     print_test("Prueba abb pertenece clave3, es true", abb_pertenece(abb, clave3));
 
