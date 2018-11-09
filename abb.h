3c31c1a4d3d6da36b9e0bd8f93f1044f6f6721d5
Milagros Cruz <mivircruz@gmail.com>
2018-11-08 22:44:31 -0300

    iteradores

--- abb.h
+++ abb.h
@@ -1,7 +1,15 @@
 #ifndef ABB_H
 #define ABB_H
 
+
+/* *****************************************************************
+ *                DEFINICION DE LOS TIPOS DE DATOS
+ * *****************************************************************/
+
 typedef struct abb abb_t;
+
+typedef struct abb_iter abb_iter_t;
+
 
 //Compara dos cadenas.
 //Pre: recibe 2 cadenas. No se modifican las cadenas recibidas.
@@ -10,7 +18,14 @@ typedef struct abb abb_t;
 //*)Entero MAYOR a 0 si la primer cadena es mayor a la segunda
 //*)Entero IGUAL a 0 si ambas cadenas son iguales
 typedef int (*abb_comparar_clave_t) (const char *, const char *);
+
+
 typedef void (*abb_destruir_dato_t) (void *);
+
+
+/* *****************************************************************
+ *                    PRIMITIVAS DEL ABB
+ * *****************************************************************/
 
 //Crea un abb.
 //Pre: se pasa por parámetro dos funciones: una para comparar cadenas y otra, para destruir datos.
@@ -21,6 +36,10 @@ abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato);
 //Pre: el abb fue creado.
 //Post: se insertó un elemento al árbol.
 bool abb_guardar(abb_t *arbol, const char *clave, void *dato);
+
+//Saca el elemento que contenga la clave pasada por parámetro. En caso de no encontrarse en el abb, se deuelve NULL.
+//Pre: el abb fue creado.
+//Post: se devolvió el dato del elemento o NULL según corresponda.
 void *abb_borrar(abb_t *arbol, const char *clave);
 
 //Devuelve el elemento en caso de estar en el abb, NULL en caso contrario.
@@ -39,5 +58,31 @@ size_t abb_cantidad(abb_t *arbol);
 //Pre: el abb fue creado.
 //Post: el abb fue destruido.
 void abb_destruir(abb_t *arbol);
+
+/* *****************************************************************
+ *                    PRIMITIVA DEL ITERADOR INTERNO
+ * *****************************************************************/
+
+//Recorre el abb aplicando la función visitar.
+//Pre: el abb y la función visitar fueron creadas. No se puede eliminar o insterar elementos mientras se recorre el abb.
+void abb_in_order(abb_t* arbol, bool visitar(const char*, void*, void*), void* extra);
+
+/* *****************************************************************
+ *                    PRIMITIVAS DEL ITERADOR EXTERNO
+ * *****************************************************************/
+
+//Crea un iterador externo que recorre el arbol.
+//Pre: el arbol fue creado.
+//Post: el iterador fue creado.
+abb_iter_t *abb_iter_in_crear(const abb_t* arbol);
+
+bool abb_iter_in_avanzar(abb_iter_t *iter);
+
+const char* abb_iter_in_ver_actual(const abb_iter_t* iter);
+
+bool abb_iter_in_al_final(const abb_iter_t* iter);
+
+void abb_iter_in_destruir(abb_iter_t* iter);
+
 
 #endif
