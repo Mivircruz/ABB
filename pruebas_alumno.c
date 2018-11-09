f3bd931e97de2931e18c50ac4e50ec3b6d9a2893
Juan B <jbenitez@fi.uba.ar>
2018-11-09 02:14:39 -0300

    Añadido pruebas de cantidad y de iteradores interno y externo(INCOMPLEt)

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -60,7 +60,7 @@ static void pruebas_abb_insertar(){
     print_test("Cantidad de elementos es 1", abb_cantidad(abb) == 1);
     print_test("Volver a insertar clave2", abb_guardar(abb, clave2, valor2));
     print_test("Volver a insertar clave1", abb_guardar(abb, clave1, valor1));
-    print_test("Borrar clave3 (raíz con dos hijos)", abb_borrar(abb, clave3) == valor3); 
+    print_test("Borrar clave3 (raíz con dos hijos)", abb_borrar(abb, clave3) == valor3);
 
 
     abb_destruir(abb);
@@ -77,7 +77,37 @@ bool insertar_preposiciones(abb_t* abb){
 	return ok;
 }
 
-void pruebas_abb_varios_elementos_recorridos(){
+bool verificar_insertados(abb_t* abb){
+	bool ok = false;
+	size_t cant_proposiciones = 23;
+	char* proposiciones[] = {"a", "ante", "bajo", "cabe", "con", "contra", "de", "desde", "durante", "en", "entre", "hacia", "hasta", "mediante", "para", "por", "segun", "sin", "so", "sobre", "tras", "versus", "via"};
+	for (size_t i = 0; i < cant_proposiciones; i++){
+		if (! (proposiciones[i] == abb_obtener(abb, proposiciones[i]) ) ){
+			ok = false;
+			break;
+		}
+		ok = true;
+	}
+	return ok;
+}
+
+bool imprimir(const char* nada, void* cadena, void* extra){
+  printf("%s;", (char*)cadena);
+	return true;
+}
+
+bool borrar_pronombres(abb_t* abb){
+	char* proposiciones[] = {"a", "ante", "bajo", "cabe", "con", "contra", "de", "desde", "durante", "en", "entre", "hacia", "hasta", "mediante", "para", "por", "segun", "sin", "so", "sobre", "tras", "versus", "via"};
+	//bool ok = false;
+	size_t cant_proposiciones = 23;
+	for (size_t i = 0; i < cant_proposiciones; i++){
+	if (!(proposiciones[i] == abb_borrar(abb,proposiciones[i]) ))
+		return false;
+	}
+	return true;
+}
+
+void pruebas_abb_varios_elementos(){
 
 	abb_t* abb = abb_crear(strcmp,NULL);
 
@@ -85,8 +115,42 @@ void pruebas_abb_varios_elementos_recorridos(){
 	ok = insertar_preposiciones(abb);
 	print_test("colocado varios elementos", ok);
 	print_test("la cantidad de pronombres colocados es 23", (abb_cantidad(abb) == 23));
+	ok = verificar_insertados(abb);
 	//verificar de alguna forma que estan ordenados
+	print_test("los elementos insertados estan presentes en el arbol", ok);
+	printf("Resultado de usar el iterador interno con imprimir;\n");
+	abb_in_order(abb,imprimir,NULL);
+	printf("\n");
+	ok = borrar_pronombres(abb);
+	print_test("Todos los pronombres fueron borrados", ok);
+	print_test("La cantidad de nodos es 0", (abb_cantidad(abb) == 0));
 	abb_destruir(abb);
+}
+
+void pruebas_abb_iter_simples(){
+	printf("\nPruebas de iterador con abb vacio\n");
+	abb_t* abb = abb_crear(NULL,NULL);
+
+	abb_iter_t* iter = abb_iter_in_crear(abb);
+	print_test("Iterador NO creado porque el abb no tiene raiz", !iter);
+	//abb_iter_in_destruir(iter);
+
+	char *clave1 = "perro", *valor1 = "guau";
+	//char *clave2 = "gato", *valor2 = "miau";
+	//char *clave3 = "vaca", *valor3 = "mu";
+
+	printf("\nPruebas de iterador con abb de un solo elemento\n");
+
+	abb_guardar(abb, clave1, valor1);
+	iter = abb_iter_in_crear(abb);
+	print_test("El iterador creado no esta en NULL", iter);
+	print_test("El valor actual al que apunta es el guardado", (abb_iter_in_ver_actual(iter) == clave1) );
+	print_test("El iterador NO esta al final", (abb_iter_in_al_final(iter) == false));
+	print_test("El iterador puede avanzar", (abb_iter_in_avanzar(iter) == true));
+	print_test("El iterador esta al final", (abb_iter_in_al_final(iter) == true));
+	print_test("El iterador NO puede avanzar", (abb_iter_in_avanzar(iter) == false));
+
+	abb_iter_in_destruir(iter);
 }
 
 /*-----------------------------------
@@ -95,5 +159,6 @@ void pruebas_abb_varios_elementos_recorridos(){
 void pruebas_abb_alumno(){
 	pruebas_abb_vacio();
 	pruebas_abb_insertar();
-	pruebas_abb_varios_elementos_recorridos();
+	pruebas_abb_varios_elementos();
+	pruebas_abb_iter_simples();
 }
